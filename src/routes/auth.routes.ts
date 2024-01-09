c7045597e4dcf4f072fb6a5f25f78126fe8d0bff
milan julinac <milanjulinac996@gmail.com>
2024-01-09 15:55:03 +0100

    added auth validation middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
+import { loginUserValidation } from "../middleware/validation/loginValidation.middleware";
 
 const router = express.Router();
 
 router.post("/register", authController.registerUser);
-router.post("/login", authController.loginUser);
+router.post("/login", loginUserValidation, authController.loginUser);
 
 export default router;
